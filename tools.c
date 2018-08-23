32202298ae7b749ff3684e6e9898647c2b786da5
Itay Cohen <itay99988@gmail.com>
2018-08-23 23:45:32 +0300

    ** Tools Module Added **
    the tools module was added and suited to the newer version of the code
    (with the board struct).

---
+++ tools.c
@@ -0,0 +1,106 @@
+/*
+ * Tools Module
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "SPBufferset.h"
+#include <string.h>
+#include <time.h>
+#include "game.h"
+#include "solver.h"
+#include "mainAux.h"
+#include "parser.h"
+
+
+void save (Board* board, char *path)
+{
+	int i, j;
+	int n,m,size;
+	FILE *f = fopen(path, "w");
+
+	/*definitions of dimentions: */
+	n=board->n, m=board->m, size=board->boardsize;
+
+	if (f == NULL)
+	{
+	    printf("Error opening file!\n"); /*what should we do???*/
+	}
+	else
+	{
+		fprintf(f, "%d %d\n", m, n);
+		for(i=0;i<size;i++)
+		{
+			for(j=0;j<size;j++)
+			{
+				fprintf(f,"%d",board->cells[i][j].value);
+				if (board->cells[i][j].fixed==1)
+					fprintf(f,".");
+				else
+					fprintf(f," ");
+				fprintf(f, " ");
+			}
+			fprintf(f,"\n");
+		}
+	}
+	fclose(f);
+}
+
+Board* load (char *path, int n, int m)
+{
+	int size, i, j;
+	Board *board;
+	FILE *f = fopen(path, "r");
+    char line[1024];
+    char * data;
+
+    char delimiters[] = " \t\r\n";
+    board = init(n,m);
+    i = 0;
+    j = 0;
+    /*printf("------------------\n");*/
+
+	if (f == NULL)
+	{
+	    printf("Error opening file!\n"); /*what should we do???*/
+	}
+	else
+	{
+		if(fgets(line, sizeof line, f) != NULL)
+		{
+			/*printf("first line is '%s'\n",line);*/
+			m = atoi(strtok(line,delimiters));
+			n = atoi(strtok(line,delimiters));
+			size = m*n;
+			board = init(n,m);
+		}
+
+		for (i=0; i<size;i++)
+		{
+			if(fgets(line, sizeof line, f) != NULL)
+			{
+				/*printf("%s\n",line);*/
+		    	/*need to get the values from the line*/
+				data = strtok(line,delimiters);
+		    	for (j=0; j<size;j++)
+		    	{
+
+		    		/*printf("%s ",data);*/
+		    		if (data[strlen(data)-1] == '.')
+		    		{
+		    			board->cells[i][j].fixed = 1;
+		    			data[strlen(data)-1] = '\0';
+		    		}
+
+		    		board->cells[i][j].value = atoi(data);
+		    		/*printf("\n");*/
+		    		data = strtok(NULL,delimiters);
+		    	}
+			}
+	    }
+	}
+	fclose(f);
+
+	return board;
+}
---
+++ tools.h
@@ -0,0 +1,7 @@
+#ifndef TOOLS_H_
+#define TOOLS_H_
+
+void save (Board* board, char *path);
+Board* load (char *path, int n, int m);
+
+#endif /* TOOLS_H_ */
