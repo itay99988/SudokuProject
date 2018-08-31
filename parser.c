df0d29d68f63769ea5258f6e66a506ebb289c189
Yotam Aharony <yotamaharony@gmail.com>
2018-08-31 12:47:18 +0300

    parser was updated (too many changes to go into details

--- parser.c
+++ parser.c
@@ -12,6 +12,9 @@
 #include "solver.h"
 #include "undoList.h"
 #include "tools.h"
+#include "mainAux.h"
+#include "solver.h"
+#include "ILPSolver.h"
 
 /*
  * read
@@ -28,7 +31,7 @@ void read()
 	 * 1 - Solve
 	 * 2 - Edit
 	 */
-	int mode = 1; /*starts in Init mode*/
+	int mode = 0; /*starts in Init mode*/
 	char input[1024]; /*maybe 256?*/
 	char *string[1024];
 
@@ -40,6 +43,9 @@ void read()
 	int solved = 0; /*'set' returns 2 if puzzle was solved*/
 	FILE *fp;
 	Board* userBoard;
+	Board* fullBoard;
+
+	const int initialBoardDimension = 3; /* according to the forum  is 3*3 - want to change it to define???*/
 
 
 	/* dimensions definition: */
@@ -81,7 +87,9 @@ void read()
 					else
 					{
 						solved = set(userBoard, undoList ,y-1,x-1,z);
-						/*implement f-i*/
+
+						if (solved == 2)
+							mode = 0; /* puzzle solved, game mode is INIT */
 					}
 				}
 				else if (strcmp(string[0],"hint")==0 && string[1]!=NULL && string[2]!=NULL && solved!=2 && mode==1) /*available only in solve*/
@@ -93,10 +101,25 @@ void read()
 								printf("Error: value not in range 0-%d\n",boardsize);
 						else if (!((x>=0 && x<=boardsize) && (y>=0 && y<=boardsize)))
 							printf("Error: value not in range 0-%d\n",boardsize);
+						else if (isThereAnError(userBoard)){
+							printf("Error: board contains erroneous values\n");
+						}
 						else
 						{
-							/*hint(generatedBoard, y-1,x-1);*/
-							/*implement e-h*/
+							if (userBoard->cells[x][y].fixed==1)
+								printf("Error: cell is fixed\n");
+							else if (userBoard->cells[x][y].value!=0)
+								printf("Error: cell already contains a value\n");
+							else
+							{
+								/*run ILP and get...*/
+								fullBoard = copyBoard(userBoard);
+								solved = ilpSolve(fullBoard);
+								if (solved==0)
+									printf("Error: board is unsolvable\n");
+								else
+									hint(fullBoard,x,y);
+							}
 						}
 					}
 
@@ -109,16 +132,12 @@ void read()
 						if(isBoardValid(userBoard))
 							printf("Validation passed: board is solvable\n");
 						else
-							printf("Validation failed: board is unsolvable\n");						/*printf("VALIDATE");*/
-
-							/*implement validate*/
+							printf("Validation failed: board is unsolvable\n");
 					}
 
 				else if (strcmp(string[0],"reset")==0 && (mode==1 || mode==2))/*available in solve or edit*/
 				{
-					/*need to update according to new rules*/
-
-					/*need to update solved to 0*/
+					reset(userBoard,undoList);
 					solved = 0;
 				}
 
@@ -126,6 +145,7 @@ void read()
 				{
 					/* implement solve*/
 					mode = 1;
+
 					fp = fopen(string[1], "r");
 					if (fp==NULL)
 					{
@@ -133,8 +153,8 @@ void read()
 					}
 					else
 					{
-						/*load*/
 						userBoard = load(string[1]);
+						boardsize = userBoard->boardsize;
 						printBoard(userBoard);
 					}
 
@@ -142,8 +162,8 @@ void read()
 				else if (strcmp(string[0],"edit")==0) /*available in every mode*/
 				{
 					/* implement edit*/
-					mode = 2;
-					userBoard ->markErrors = 1;
+					mode = 2; /* start a puzzle in edit mode */
+
 					if (string[1]!=NULL) /*there is a parameter*/
 					{
 						fp = fopen(string[1], "r");
@@ -152,13 +172,21 @@ void read()
 							printf("Error: File doesn't exist or cannot be opened\n");
 						}
 						else
-							/*need to call to Solve\load*/
+						{
 							userBoard = load(string[1]);
+							userBoard->markErrors = 1;/* mark errors parameter is 1 */
+							boardsize = userBoard->boardsize;
+							printBoard(userBoard);
+						}
+
 					}
 					else
 					{
 						/* need to initilalize an empty board */
-
+						userBoard = init(initialBoardDimension, initialBoardDimension); /* initiate 3*3 */
+						userBoard->markErrors = 1;/* mark errors parameter is 1 */
+						boardsize = userBoard->boardsize;
+						printBoard(userBoard);
 					}
 				}
 				else if (strcmp(string[0],"mark_errors")==0 && string[1]!=NULL && mode==1) /*available only in solve*/
@@ -174,6 +202,7 @@ void read()
 				else if (strcmp(string[0],"print_board")==0 && (mode==1 || mode==2)) /*available in solve or edit*/
 				{
 					/* implement printBoard*/
+					printBoard(userBoard);
 				}
 				else if (strcmp(string[0],"generate")==0 && string[1]!=NULL && string[2]!=NULL && mode==2) /*available only in edit*/
 				{
@@ -201,34 +230,83 @@ void read()
 				}
 				else if (strcmp(string[0],"undo")==0 && (mode==1 || mode==2)) /*available in solve or edit*/
 				{
-					/* implement undo*/
+					undo(userBoard,undoList);
 				}
 				else if (strcmp(string[0],"redo")==0 && (mode==1 || mode==2)) /*available in solve or edit*/
 				{
-					/* implement redo*/
+					redo(userBoard,undoList);
 				}
 				else if (strcmp(string[0],"save")==0 && string[1]!=NULL && (mode==1 || mode==2)) /*available in solve or edit*/
 				{
-					/* implement save*/
+					if (mode==1)
+					{
+						save(userBoard, string[1]);
+						printf("Saved to: %s\n", string[1]);
+					}
+					else /*mode==2*/
+					{
+						if(isThereAnError(userBoard))
+							printf("Error:board contains erroneous values\n");
+
+						if(isBoardValid(userBoard))
+						{
+							save(userBoard, string[1]);
+							printf("Saved to: %s\n", string[1]);
+						}
+						else
+							printf("Error: board validation failed\n");
+
+						markAsFixed(userBoard); /*implement I - check if this is the acctual need*/
+
+					}
+
+
 				}
 				else if (strcmp(string[0],"num_solutions")==0 && (mode==1 || mode==2))/*available in solve or edit*/
 				{
-					/* implement c-f*/
+					int numSolutions;
+					if(isThereAnError(userBoard))
+						printf("Error: board contains erroneous values\n");
+					else
+					{
+						numSolutions=getNumSolutions(userBoard);
+						printf("Number of solutions: %d\n", numSolutions);
+
+
+						if(numSolutions==1)
+							printf("This is a good board!\n");
+						else
+							printf("The puzzle has more than 1 solution, try to edit it further\n");
+					}
+
 				}
 				else if ((strcmp(string[0],"autofill")==0) && (mode==1)) /*available only in solve*/
 				{
-					/* implement c-g*/
+					if(isThereAnError(userBoard))
+						printf("Error: board contains erroneous values\n");
+					else
+					{
+						autoFill(userBoard,undoList);
+						printBoard(userBoard);
+					}
 				}
 				else if (strcmp(string[0],"exit")==0) /*available in every mode*/
 				{
 					/* implement exit*/
-					exitGame(userBoard);
+					exitGame(userBoard, undoList);
 				}
 				else
 				{
 					printf("Error: invalid command\n");
-					printf("Enter your command:\n");
+					/*invalid = 1;*/
 				}
+
+			  	printf("Enter your command:\n");
+			  	/*
+			  	if (!invalid)
+			  		printBoard(userBoard);
+
+			  	invalid = 0;*/ /*reset the invalid*/
 		  }
 
 		  i=0;
@@ -245,13 +323,13 @@ void read()
 		  else
 		  {
 			  /*GOT EOF, therefore exits*/
-			  exitGame(userBoard);
+			  exitGame(userBoard, undoList);
 		  }
 
 	  }
 
 	  /*NEED TO EXIT*/
-	  exitGame(userBoard);
+	  exitGame(userBoard, undoList);
 
 }
 
